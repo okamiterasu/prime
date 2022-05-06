6eda5a7c19730c7e3ba0df70928a3910fa9d8c34
Brian Kram <bkram@usca.edu>
2022-05-06 17:53:30 -0400

    split group off into function

--- src/ui.rs
+++ src/ui.rs
@@ -70,60 +70,74 @@ impl eframe::App for App
 				if let Some(i)=self.to_remove.take(){self.tracked.remove(i);}
 				for (i, tracked) in self.tracked.iter().enumerate()
 				{
-					ui.group(|ui|
-					{
-						ui.vertical(|ui|
-						{
-							if ui.button("Del").clicked()
-							{
-								self.to_remove = Some(i);
-							}
-							ui.heading(tracked.common_name.as_ref().unwrap_or(&tracked.unique_name));
-							ui.horizontal(|ui|
-							{
-								let owned = match self.owned.get_mut(&tracked.recipe.unique_name)
-								{
-									Some(v)=>v,
-									None=>self.owned.entry(tracked.recipe.unique_name.clone()).or_insert(0)
-								};
-								let color = if *owned>=1 {egui::Color32::BLACK} else {ui.visuals().text_color()};
-								if ui.button("-").clicked()
-								{
-									*owned = owned.saturating_sub(1);
-								}
-								if ui.button("+").clicked()
-								{
-									*owned += 1;
-								}
-								ui.colored_label(color, format!("{} of {}", owned, 1));
-								ui.colored_label(color, tracked.recipe.common_name.as_ref().unwrap_or(&tracked.recipe.unique_name));
-							});
-							for component in &tracked.components
-							{
-								ui.horizontal(|ui|
-								{
-									let owned = match self.owned.get_mut(&component.unique_name)
-									{
-										Some(v)=>v,
-										None=>self.owned.entry(component.unique_name.clone()).or_insert(0)
-									};
-									let color = if *owned>=component.count {egui::Color32::BLACK} else {ui.visuals().text_color()};
-									if ui.button("-").clicked()
-									{
-										*owned = owned.saturating_sub(1);
-									}
-									if ui.button("+").clicked()
-									{
-										*owned += 1;
-									}
-									ui.colored_label(color, format!("{} of {}", owned, component.count));
-									ui.colored_label(color, component.common_name.as_ref().unwrap_or(&component.unique_name));
-								});
-							}
-						});
-					});
+					recipe_group(ui, tracked, i, &mut self.owned, &mut self.to_remove);
 					if i%7==6{ui.end_row()}
 				}
+			});
+		});
+	}
+}
+
+fn recipe_group(
+	ui: &mut Ui,
+	tracked: &Tracked,
+	i: usize,
+	owned: &mut HashMap<String, u32>,
+	to_remove: &mut Option<usize>) -> egui::InnerResponse<()>
+{
+	ui.group(|ui|
+	{
+		ui.vertical(|ui|
+		{
+			if ui.button("Del").clicked()
+			{
+				*to_remove = Some(i);
+			}
+			ui.heading(tracked.common_name.as_ref().unwrap_or(&tracked.unique_name));
+			ui.horizontal(|ui|
+			{
+				let owned = match owned.get_mut(&tracked.recipe.unique_name)
+				{
+					Some(v)=>v,
+					None=>owned.entry(tracked.recipe.unique_name.clone()).or_insert(0)
+				};
+				let color = if *owned>=1 {egui::Color32::BLACK} else {ui.visuals().text_color()};
+				if ui.button("-").clicked()
+				{
+					*owned = owned.saturating_sub(1);
+				}
+				if ui.button("+").clicked()
+				{
+					*owned += 1;
+				}
+				ui.colored_label(color, format!("{} of {}", owned, 1));
+				ui.colored_label(color, tracked.recipe.common_name.as_ref().unwrap_or(&tracked.recipe.unique_name));
+			});
+			for component in &tracked.components
+			{
+				ui.horizontal(|ui|
+				{
+					let owned = match owned.get_mut(&component.unique_name)
+					{
+						Some(v)=>v,
+						None=>owned.entry(component.unique_name.clone()).or_insert(0)
+					};
+					let color = if *owned>=component.count {egui::Color32::BLACK} else {ui.visuals().text_color()};
+					if ui.button("-").clicked()
+					{
+						*owned = owned.saturating_sub(1);
+					}
+					if ui.button("+").clicked()
+					{
+						*owned += 1;
+					}
+					ui.colored_label(color, format!("{} of {}", owned, component.count));
+					ui.colored_label(color, component.common_name.as_ref().unwrap_or(&component.unique_name));
+				});
+			}
+		});
+	})
+}
 			});
 		});
 	}
