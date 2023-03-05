424e3cfd9f0197426df527571ffbf91b697161a2
Brian Kram <bkram@usca.edu>
2023-03-05 16:24:44 -0500

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,18 @@ mod ui;
 #[cfg(target_os = "windows")]
 fn cache_dir() -> Result<PathBuf>
 {
-	let home = std::env::var("UserProfile")?;
-	let mut path = PathBuf::from(home);
-	path.push("primes/");
+	let home = std::env::var("UserProfile")
+		.map(PathBuf::from)?;
+	let path = home.join("primes/");
 	Ok(path)
 }
 
 #[cfg(target_os = "linux")]
 fn cache_dir() -> Result<PathBuf>
 {
-	let home = std::env::var("HOME")?;
-	let mut path = PathBuf::from(home);
-	path.push(".cache/primes/");
+	let home = std::env::var("HOME")?
+		.map(PathBuf::from);
+	let path = home.push(".cache/primes/");
 	Ok(path)
 }
 
@@ -83,11 +83,13 @@ fn main() -> Result<()>
 	if updated
 	{
 		let droptable_path = cache_dir.join("droptable.html");
-		let droptable = live::droptable().context("Downloading scrape droptable")?;
+		let droptable = live::droptable()
+			.context("Downloading scrape droptable")?;
 		std::fs::write(droptable_path, droptable)?;
 
 		let worldstate_path = cache_dir.join("worldstate.json");
-		let worldstate = live::worldstate().context("Downloading world state")?;
+		let worldstate = live::worldstate()
+			.context("Downloading world state")?;
 		std::fs::write(worldstate_path, worldstate)?;
 	}
 
@@ -116,13 +118,13 @@ fn update_index_if_needed(dir: &Path) -> Result<bool>
 {
 	let index_path = dir.join("index_en.txt.lzma");
 	let six_hours_ago = SystemTime::now() - SIX_HOURS;
-	let index_out_of_date = std::fs::File::open(&index_path)
+	let index_maybe_out_of_date = std::fs::File::open(&index_path)
 		.and_then(|file|file.metadata())
 		.and_then(|metadata|metadata.modified())
 		.map(|last_modified|last_modified < six_hours_ago)
 		.unwrap_or(true);
 
-	if index_out_of_date
+	if index_maybe_out_of_date
 	{
 		let index = live::index()
 			.context("Downloading new index")?;
@@ -133,7 +135,7 @@ fn update_index_if_needed(dir: &Path) -> Result<bool>
 		let parsed_index = cache::load_index(dir)?;
 		remove_old_manifests(dir, &parsed_index)?;
 	}
-	Ok(index_out_of_date)
+	Ok(index_maybe_out_of_date)
 }
 
 fn remove_old_manifests(dir: &Path, index: &HashMap<String, String>) -> Result<()>
@@ -147,6 +149,7 @@ fn remove_old_manifests(dir: &Path, index: &HashMap<String, String>) -> Result<(
 		if file_name.starts_with("Export")
 			&& file_name != index[&file_name[0..file_name.len()-26]]
 		{
+			println!("Deleting stale manifest: {file_name}");
 			std::fs::remove_file(file.path())
 				.with_context(||format!("Deleting file: {:?}", file.file_name()))?;
 		}
